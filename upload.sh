333235f7ca3f525694bdda763f68ff447a4d459b
Finn Callies <finn.callies@gmail.com>
2025-04-18 12:25:08 +0200

    Auto-commit: 2025-04-18 12:25:08

--- upload.sh
+++ upload.sh
@@ -4,24 +4,24 @@ LOG=$HOME/.var/log/autogit/$(date '+%d-%m-%Y').log
 
 # Navigate to the repository path, if provided as argument
 if [ -n "$1" ]; then
-  cd "$1" || { echo "[ autogit ] $1: Repository path '$1' not found." >> $LOG; exit 1; }
+  cd "$1" || { echo "$(date '+%H:%M:%S') [ autogit ] $1: Repository path '$1' not found." >> $LOG; exit 1; }
 fi
 
 # Check if current directory is a git repository
 if [ ! -d .git ]; then
-  echo "[ autogit ] $1: $1 not a git repository." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: $1 not a git repository." >> $LOG
   exit 1
 fi
 
 # check remote information
 git fetch
 if [ $? -ne 0 ]; then
-  echo "[ autogit ] $1: fetching remote repository information failed." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: fetching remote repository information failed." >> $LOG
 fi
 
 # Check for uncommitted changes
 if [[ -n $(git status --porcelain) ]]; then
-  echo "[ autogit ] $1: Uncommitted changes found." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: Uncommitted changes found." >> $LOG
 
   # Add all changes
   git add -A
@@ -32,9 +32,9 @@ if [[ -n $(git status --porcelain) ]]; then
   # Commit with timestamp message
   git commit -m "Auto-commit: $timestamp"
 
-  echo "[ autogit ] $1: Changes commited." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: Changes commited." >> $LOG
 else
-  echo "[ autogit ] $1: No uncommitted changes." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: No uncommitted changes." >> $LOG
 fi
 
 git status -sb | grep "ahead" --quiet
@@ -43,8 +43,8 @@ if [[ $? -eq 0 ]]; then
   branch=$(git symbolic-ref --short HEAD)
   git push origin "$branch"
 
-  echo "[ autogit ] $1: Changes pushed to branch '$branch'." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: Changes pushed to branch '$branch'." >> $LOG
 else
-  echo "[ autogit ] $1: Nothing to push." >> $LOG
+  echo "$(date '+%H:%M:%S') [ autogit ] $1: Nothing to push." >> $LOG
 fi
 
